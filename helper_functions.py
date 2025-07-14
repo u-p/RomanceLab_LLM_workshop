7fb7dc7bb9218bce50e96d964c86122299d001b6
u-p <umesh.patil@gmail.com>
2025-07-14 15:03:19 +0200

    file updates

--- helper_functions.py
+++ helper_functions.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-def plot_surprisals(surprisals: list, ymax=None, bar_col=None):
+def plot_surprisals(surprisals: list, ymax=None, bar_col=None, title=None):
     tokens, surprisal_vals = zip(*surprisals[0])
 
     if ymax is None:
@@ -10,11 +10,14 @@ def plot_surprisals(surprisals: list, ymax=None, bar_col=None):
     if bar_col is None:
         bar_col = "#1f77b4"
 
+    if title is None:
+        title = "Surprisal for each input"
+
     plt.figure(figsize=(8, 4))
     plt.bar(tokens, surprisal_vals, color=bar_col, width=0.9)
     plt.xlabel('')
     plt.ylabel('')
-    plt.title('Surprisal for each input')
+    plt.title(title)
     plt.xticks(rotation=45)
     plt.ylim(0, ymax)
     plt.tight_layout()
